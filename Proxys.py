2a42189616d1346d539c5fe490bf9714b897793d
Chris-Smile7 <Chris-Chirs@outlook.com>
2024-10-14 23:36:58 +0800

    update

--- Proxys.py
+++ Proxys.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 
 PROXY_DIST_FILE="ProxyList"
+TEST_URL="https://www.google.com/ncr"
 
 Proxy_Type=np.dtype([
     ('proto','str_',10),
@@ -15,9 +16,25 @@ def loadProxyList(path: str) -> np.ndarray:
     proxys=np.load(PROXY_DIST_FILE)
     return proxys
 
-def checkProxy():
-    pass
+def checkProxy(proto: str, address: str, port: int) -> bool:
+    URL=f"{proto}://{address}:{port}"
+    res=requests.get(url=TEST_URL,headers={
+        "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding":"gzip, deflate, br, zstd",
+        "accept-language":"zh-CN,zh;q=0.9",
+        "connection":"keep-alive",
+        "sec-ch-ua":'"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+        "sec-ch-ua-mobile":"?0",
+        "sec-ch-ua-platform":'"Windows"',
+        "sec-fetch-dest":"document",
+        "sec-fetch-mode":"navigate",
+        "sec-fetch-site":"none",
+        "sec-fetch-user":"?1",
+        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+    },proxies={
+        'https':URL
+    })
+    return res.status_code==200
 
 if __name__=='__main__':
-    a=np.array([('http','localhost',8888)],dtype=Proxy_Type)
-    np.savez_compressed("ProxyList",a)
+    
