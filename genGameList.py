4b63f0c3544ab17a475bc1f82b57e86c24c1fa8b
Chris-Smile7 <Chris-Chirs@outlook.com>
2024-10-17 17:29:50 +0800

    pre add some data.

---
+++ genGameList.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import re,json
+
+
+async def genGameList(string: str):
+
+    Uri_matcher=re.compile("/app/([0-9]*)/charts/")
+
+    def match_href(href: str):
+        return href and Uri_matcher.search(href)
+
+    def match_string(string: str):
+        return string and "<" not in string
+
+    soup=BeautifulSoup(string,"lxml")
+    games=soup.find_all(href=match_href,string=match_string)
+    gamesList=[]
+    for game in games:
+        gamesList.append({
+            'appid': int(game['href'][5:-8]),
+            'name': game.string,
+            'allTimePeak': int(game.parent.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string.replace(",","")),
+            'done': False
+        })
+
+    gamesList.sort(key=lambda game:game['allTimePeak'],reverse=True)
+    gamesList=gamesList[:1000]
+
+    with open("data.json","w") as f:
+        json.dump(gamesList,f,indent=4)
+
+    
---
+++ main.py
@@ -0,0 +1,56 @@
+import asyncio,random,atexit
+import nodriver as uc
+
+from genGameList import *
+
+DATA_FOLDER="data"
+BaseUrl='https://steamdb.info/'
+
+async def getGameList():
+    browser = await uc.start()
+    page = await browser.get(BaseUrl+"charts/")
+    await page.sleep(5)
+    while True:
+        s=await page.get_content()
+        if "等待" in s:
+            await page.sleep(2)
+        else:
+            break
+    print("enter!")
+    await genGameList(await page.get_content())
+    print("已生成data.json")
+
+async def getData(dataPath: str):
+    with open(dataPath,"r") as f:
+        gamesList=json.load(f)
+    browser = await uc.start()
+    page = await browser.get(BaseUrl)
+    await page.sleep(5)
+    while True:
+        s=await page.get_content()
+        if "等待" in s:
+            await page.sleep(2)
+        else:
+            break
+    print("enter!")
+    await asyncio.sleep(random.uniform(2.0,5.0))
+    def onExit():
+        with open(dataPath,"w") as f:
+            json.dump(gamesList,f,indent=4)
+    atexit.register(onExit)
+    try:
+        for game in gamesList:
+            if game['done']:
+                continue
+            await page.get(BaseUrl+f"api/GetGraphMax/?appid={game['appid']}")
+            with open(f"{DATA_FOLDER}/{game['appid']}.json","w") as f:
+                f.write((await page.get_content())[99:-64])
+            game['done']=True
+            await asyncio.sleep(random.uniform(15.0,20.0))
+    except:
+        print("Error")
+    
+
+if __name__ == '__main__':
+    # since asyncio.run never worked (for me)
+    uc.loop().run_until_complete(getData("data.json"))
