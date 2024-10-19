31d532e00e244c18396a4dabdbb0327aefa32607
Chris-Smile7 <Chris-Chirs@outlook.com>
2024-10-20 02:49:27 +0800

    update

--- main.py
+++ main.py
@@ -1,70 +1,97 @@
-import asyncio,random,atexit
+import asyncio,random,atexit,queue
 import nodriver as uc
 
 from genGameList import *
 
+
 DATA_FOLDER="data"
 BaseUrl='https://steamdb.info/'
 
-async def getGameList():
-    browser = await uc.start()
-    page = await browser.get(BaseUrl+"charts/?sort=peak")
-    await page.sleep(5)
-    while True:
-        s=await page.get_content()
-        if "等待" in s:
-            await page.sleep(2)
-        else:
-            break
-    print("enter!")
-    await asyncio.sleep(10)
-    await genGameList(await page.get_content())
-    print(await page.get_content())
-    print("已生成data.json")
+class Scraper:
+    def __init__(self):
+        self.lock=asyncio.Lock()
 
-async def getData(dataPath: str,max: bool):
-
-    category=("max" if max else "week")
-    apiCategory=("GetGraphMax" if max else "GetGraphWeek")
-
-    with open(dataPath,"r") as f:
-        gamesList=json.load(f)
-    browser = await uc.start()
-    page = await browser.get(BaseUrl)
-    await page.sleep(5)
-    while True:
-        s=await page.get_content()
-        if "等待" in s:
-            await page.sleep(2)
-        else:
-            break
-    print("enter!")
-    await asyncio.sleep(random.uniform(2.0,5.0))
-    def onExit():
-        with open(dataPath,"w") as f:
-            json.dump(gamesList,f,indent=4)
-    atexit.register(onExit)
-    l=len(gamesList)
-    for idx,game in enumerate(gamesList):
-        try:
-        
-            if game['done'+category]:
-                continue
-            await page.get(BaseUrl+f"api/"+apiCategory+f"/?appid={game['appid']}")
-            with open(f"{DATA_FOLDER}/"+category+f"/{game['appid']}.json","w") as f:
-                s=(await page.get_content())[99:-64]
-                res=json.loads(s)
-                if res['success']!=True:
-                    raise SyntaxError
-                f.write((await page.get_content())[99:-64])
-            game['done']=True
-            print(f"{idx} / {l}",end="\r")
-            await asyncio.sleep(random.uniform(15.0,20.0))
-        except Exception as e:
-            print(f"Eror: Index: {idx} Appid: {game['appid']}  Name: {game['name']}")
     
+
+    async def handler(self, event: uc.cdp.network.ResponseReceived):
+        if self.apiCategory not in event.response.url:
+            return
+        print(event.request_id)
+        async with self.lock:
+            self.requestQueue.put(event.request_id)
+
+    async def getGameList():
+        browser = await uc.start()
+        page = await browser.get(BaseUrl+"charts/?sort=peak")
+        await page.sleep(5)
+        while True:
+            s=await page.get_content()
+            if "等待" in s:
+                await page.sleep(2)
+            else:
+                break
+        print("开始爬取.")
+        await asyncio.sleep(5)
+        await genGameList(await page.get_content())
+        print(await page.get_content())
+        print("已生成 data.json 文件.")
+
+    async def getData(self, dataPath: str, max: bool):
+        self.category=("max" if max else "week")
+        self.apiCategory=("GetGraphMax" if max else "GetGraphWeek")
+        self.requestQueue=queue.Queue()
+        with open(dataPath,"r") as f:
+            gamesList=json.load(f)
+        length=len(gamesList)
+        browser = await uc.start()
+        page = await browser.get(BaseUrl)
+        self.tab=page
+        await page.sleep(5)
+        while True:
+            s=await page.get_content()
+            if "等待" in s:
+                await page.sleep(2)
+            else:
+                break
+        print("开始爬取.")
+
+        def onExit():
+            with open(dataPath,"w") as f:
+                json.dump(gamesList,f,indent=4)
+        atexit.register(onExit)
+
+        await asyncio.sleep(2)
+        page.add_handler(uc.cdp.network.ResponseReceived,self.handler)
+        for idx,game in enumerate(gamesList):
+            try:
+                if game['done'+self.category]:
+                    continue
+                await page.get(f"https://steamdb.info/app/{game['appid']}/charts/")
+                with open(f"{DATA_FOLDER}/"+self.category+f"/{game['appid']}.json","w") as f:
+                    while True:
+                        async with self.lock:
+                            if not self.requestQueue.empty():
+                                request_id=self.requestQueue.get_nowait()
+                                command=uc.cdp.network.get_response_body(request_id=request_id)
+                                body,b64_encoded = await page.send(command)
+                                print(body)
+                                break
+                        await asyncio.sleep(0.5)
+                    res=json.loads(body)
+                    if res['success']!=True:
+                        raise SyntaxError
+                    f.write(body)
+                game['done'+self.category]=True
+                print(f"{idx} / {length}",end="\r")
+                await asyncio.sleep(random.uniform(15.0,20.0))
+            except Exception as e:
+                print(f"Eror: Index: {idx} Appid: {game['appid']}  Name: {game['name']}")
+    
+
+                
+        
 
 if __name__ == '__main__':
     # since asyncio.run never worked (for me)
     #uc.loop().run_until_complete(getGameList())
-    uc.loop().run_until_complete(getData("data.json",1))
+    uc.loop().run_until_complete(Scraper().getData("data.json",1))
